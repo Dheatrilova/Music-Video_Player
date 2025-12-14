eafbdc39d2e175e05b3d55e02ad9191d84d20a28
Dheatrilova <dheatrilova457@gmail.com>
2025-12-14 22:10:46 +0700

    Update video_player_page.dart

--- lib/video_page.dart
+++ lib/video_page.dart
@@ -1,14 +1,97 @@
 import 'package:flutter/material.dart';
+import 'video_player_page.dart';
+import 'audio_page.dart';
+import 'search_page.dart';
+import 'history_page.dart';
+import 'splash_screen.dart';
 
-class VideoPage extends StatelessWidget {
-  const VideoPage({super.key});
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Halaman Video',
-        style: TextStyle(color: Colors.white, fontSize: 20),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => const MyHomePage(title: 'ViAuo'),
+      },
+      title: 'ViAuo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 54, 45, 94),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages =[
+    VideoPlayerPage(),
+    AudioPage(),
+    SearchPage(),
+    HistoryPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 34, 27, 68),
+        leading: const Icon(Icons.person, color: Colors.white),
+        title: Text(widget.title, style: const TextStyle(color: Colors.white)),
+        centerTitle: true,
+        actions: const [Icon(Icons.more_vert, color: Colors.white)],
+        
+      ),
+
+      body: _pages[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromARGB(255, 34, 27, 68),
+        selectedItemColor: Color(0xFF3713EC),
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },        
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_call),
+            label: 'Video',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.audiotrack),
+            label: 'Audio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Telusuri',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Riwayat',
+          ),
+        ],
       ),
     );
   }
