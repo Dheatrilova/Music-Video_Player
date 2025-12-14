773d7014df734fac9a52563577b1ff4eebbcca81
Nainggolan <nainggolanripaldo07@gmail.com>
2025-12-14 12:41:45 +0700

    pengerjaan beranda dasar

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'ViAuo',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFF131022),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 54, 45, 94),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ViAuo'),
     );
   }
 }
@@ -30,41 +30,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 34, 27, 68),
 
-        backgroundColor: Color(0xFF1310222),
-        
-        title: Text(widget.title),
+        leading: const Icon(Icons.person, color: Colors.white),
+
+        title: Text(widget.title, style: const TextStyle(color: Colors.white)),
+
+        centerTitle: true,
+
+        actions: const [Icon(Icons.more_vert, color: Colors.white)],
       ),
-      body: Center(
-        
+
+      body: const Center(
         child: Column(
-        
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:',style: TextStyle(color: Colors.white),),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
